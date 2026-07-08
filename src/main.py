0b608b9d85df6c5e467eeed0cf9d55bc065fae0d
Theodor-Ar <195936609+Theodor-Ar@users.noreply.github.com>
2026-07-08 22:11:53 +0300

    in the process of creating a bot layout

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
 """Точка входа: инициализация бота, запуск поллинга"""
+
+from config.config import BOT_TOKEN
+from handlers.routes import router
+
+from aiogram import Bot, Dispatcher
+import asyncio
+
+
+dp = Dispatcher()
+dp.include_router(router)
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
